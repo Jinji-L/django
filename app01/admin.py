a2f2642d0081fed88217a4e78415887220b17de0
Jinji-L <94270330+Jinji-L@users.noreply.github.com>
2022-02-22 14:11:55 +0800

    Add files via upload

---
+++ app01/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from app01 import models
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['login_id', 'login_type','name']
+    fieldsets = (
+        (None, {'fields': ('login_id','login_type','password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('login_id','login_type', 'password1', 'password2')
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Photo)
